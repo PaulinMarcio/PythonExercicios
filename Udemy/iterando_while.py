1aa2caf6edfcdca4a756bd17977a2a368ae16555
Márcio Paulin <marcio.paulin@warren.com.br>
2023-06-07 16:35:06 -0300

    Iteração com While

--- Udemy/iterando_while.py
+++ Udemy/iterando_while.py
@@ -1,10 +1,20 @@
-nome = "Márcio"
+frase = 'O Python é uma linguagem de programação multiparadigma. Python foi criado por Guido van Rossum.'.lower()
 
-tamanho_nome = len(nome)
-print(nome)
-print(tamanho_nome)
+i = 0
+qtd = 0
+letra = ''
+while i < len(frase):
+    letra_atual = frase[i]
 
-contador = 0
-while contador < tamanho_nome:
-    print(nome[contador])
-    contador += 1
+    if letra_atual == ' ':
+        i += 1
+        continue
+
+    quantas_vezes = frase.count(letra_atual)
+    if qtd < quantas_vezes:
+        qtd = quantas_vezes
+        letra = letra_atual
+
+    i += 1
+
+print(f'A letra que apareceu mais vezes foi {letra}, aparecendo {qtd} vezes')

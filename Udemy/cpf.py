5d27bf84f6dbaf37bb1ed5afb9795149e09b3e43
Márcio Paulin <marcio.paulin@warren.com.br>
2023-06-12 17:27:54 -0300

    WIP - Primeiro Digito CPF

--- Udemy/cpf.py
+++ Udemy/cpf.py
@@ -25,15 +25,28 @@ O primeiro dígito do CPF é 7
 """
 
 cpf_mult = [10, 9, 8, 7, 6, 5, 4, 3, 2]
-cpf_num = [7, 4, 6, 8, 2, 4, 8, 9, 0]
+cpf = input('insira o seu cpf: ')
 cpf_res = 0
 
+cpf_num = cpf # não mexe no cpf original
+cpf_num = cpf_num.replace('.', '') # remove os pontos
+cpf_num = cpf_num.replace('-', '') # remove o traço
+
+try:
+    cpf_num = cpf_num.replace("", ",").lstrip(',').rstrip(',').split(',') # adiciona as virgulas entre os números e separa eles
+    for num in range(len(cpf_num)):
+        cpf_num[num] = int(cpf_num[num]) # transforma de string para int
+except Exception: 
+    print("Erro")
+
 for num in range(len(cpf_mult)):
-    cpf_mult_res = cpf_mult[num] * cpf_num[num]
-    cpf_res += cpf_mult_res
+    cpf_mult_res = cpf_mult[num] * cpf_num[num] # multiplicação de cada número
+    cpf_res += cpf_mult_res # soma dos resultados das multiplicações
     print(f'{cpf_mult[num]} * {cpf_num[num]} = {cpf_mult_res}')
 
-cpf_res = cpf_res * 10
-cpf_res = cpf_res % 11
+cpf_res = cpf_res * 10 # resultado multiplicado por 10
+cpf_res = cpf_res % 11 # e tirado o resto da divisão por 11
 
-cpf_res = 0 if cpf_res > 9 else cpf_res
+cpf_res = 0 if cpf_res > 9 else cpf_res # se o resultado for maior que 9, transformar em zero
+
+print(cpf_res) # primeiro digito do cpf

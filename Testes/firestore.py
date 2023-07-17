43c6cb147c4aafa38bf0a6b0e6733fc9dca07727
Márcio Paulin <marcio.paulin@warren.com.br>
2023-07-17 13:36:53 -0300

    aaaaaaaa

--- Testes/firestore.py
+++ Testes/firestore.py
@@ -1,41 +1,11 @@
-import firebase_admin
-from firebase_admin import firestore
-from firebase_admin import credentials
-from firebase_admin import auth
+import requests
+from google.oauth2 import service_account
+import googleapiclient.discovery
 
-cred = credentials.Certificate("C:/Users/marcio.paulin_warren/Documents/GitHub/Python/PythonExercicios/Testes/firteste.json")
-app = firebase_admin.initialize_app(cred)
+SCOPES = ['https://www.googleapis.com/auth/firebase.readonly']
+SERVICE_ACCOUNT_FILE = 'C:/Users/marcio.paulin_warren/Documents/GitHub/Python/PythonExercicios/Testes/fir-test.json'
 
-db = firestore.client(app)
+credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+firebase = googleapiclient.discovery.build('firestore','v1beta1', credentials=credentials)
 
-docs_users = db.collection("users").stream()
-docs_groups = db.collection("groups").stream()
-docs_roles = db.collection("roles").stream()
-docs_authorization = db.collection("authorization").stream()
-
-print(docs_users, docs_groups, docs_roles, docs_authorization)
-
-users = []
-    
-for doc in docs_users:
-    users.append(doc.to_dict())
-
-for doc in docs_groups:
-    users.append(doc.to_dict())
-
-for doc in docs_authorization:
-    users.append(doc.to_dict())
-
-for doc in docs_roles:
-    users.append(doc.to_dict())
-
-
-print(users)
-page = auth.list_users()
-print(page)
-while page:
-    for user in page.users:
-        email = auth.get_user(user.uid)
-        email = email.email
-        print('User: ' + email)
-    page = page.get_next_page()
+print(credentials)

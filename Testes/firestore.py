d0fad52964065fb04d71878c5744a8e86db05775
Márcio Paulin <marcio.paulin@warren.com.br>
2023-07-21 13:27:27 -0300

    Firestore wip

--- Testes/firestore.py
+++ Testes/firestore.py
@@ -1,11 +1,11 @@
-import requests
+import requests, json
 from google.oauth2 import service_account
 from google.auth.transport.requests import AuthorizedSession
-API_KEY = 'AIzaSyAoWF9e6mI-2Piz5WBxTHbuu2bGYKyu7_w'
-url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=' + API_KEY
-data = {"email": 'paulin.marcio@outlook.com', "password": 'Haineko33', "returnSecureToken": True}
-res = requests.post(url, data)
-id_token = res.json().get("idToken")
+# API_KEY = 'AIzaSyAoWF9e6mI-2Piz5WBxTHbuu2bGYKyu7_w'
+# url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=' + API_KEY
+# data = {"email": 'paulin.marcio@outlook.com', "password": 'Haineko33', "returnSecureToken": True}
+# res = requests.post(url, data)
+# id_token = res.json().get("idToken")
 
 def get_auth():
 
@@ -16,12 +16,25 @@ def get_auth():
         )
     return AuthorizedSession(credentials)
 
+	
+
 def get_users(auth):
+    users = []
     base_url = "https://firestore.googleapis.com"
-    url = base_url + "/v1/projects/fir-test-automation/databases/(default)/documents"
+    url = base_url + "/v1/projects/fir-test-automation/databases/(default)/documents/users"
 
     res = auth.get(url)
     res_body = res.json()
-    print(res_body)
+    for user in res_body["documents"]:
+        email = user["name"]
+        email = email[65:]
+        users.append(email)
+    print(users)
+
+    url_email = "https://identitytoolkit.googleapis.com/v1/accounts:lookup"
+    for email in users:
+        res = requests.post(url_email, data=email)
+        res_body = res.json()
+        print(res.json())
 
 get_users(get_auth())
